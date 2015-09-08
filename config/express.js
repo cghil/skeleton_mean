0a36c4792cad1fd6d74e8783ebc73d88a144b7e4
cghil <cghilarducci@gmail.com>
2015-09-08 13:48:15 -0700

    environment dependencies

--- config/express.js
+++ config/express.js
@@ -1,7 +1,34 @@
-var express = require('express');
+var express = require('express'),
+	morgan = require('morgan'),
+	compress = require('compression'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override');
 
 module.exports = function(){
 	var app = express();
+
+// process.env.NODE_ENV is used to determine the environment and configure the Express app accordingly
+
+
+	if (process.env.NODE_ENV === 'development') {
+
+		app.use(morgan('dev'));
+
+	} else if (process.env.NODE_ENV === 'production'){
+		// compress in production
+		app.use(compress());
+
+	}
+
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+
+	app.use(bodyParser.json());
+
+	app.use(methodOverride());
+
 	require('../app/routes/index.server.routes.js')(app);
+
 	return app;
 };
