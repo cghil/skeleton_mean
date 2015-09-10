e4f3cde3268d7fe36270a1da63412fc3d283373b
cghil <cghilarducci@gmail.com>
2015-09-10 16:37:22 -0700

    create, list, read, userByID, update, User schema

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,15 @@
+var users = require('../../app/controllers/users.server.controller');
+
+module.exports = function(app){
+	app.route('/users')
+		.post(users.create)
+		.get(users.list);
+
+	app.route('/users/:userId')
+		.get(users.read)
+		.put(users.update);
+
+	app.param('userId', users.userByID);
+};
+
+// this is going to use the controller for
