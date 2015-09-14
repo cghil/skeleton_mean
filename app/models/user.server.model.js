664e0358a447dee298f8fe39808ef566b722439a
cghil <cghilarducci@gmail.com>
2015-09-14 16:36:35 -0700

    updated schema for users, added delete to controller

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,58 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
-	username: String,
-	password: String
+	email: {
+		type: String,
+		index: true,
+		match: /.+\@.+\..+/
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true	
+	},
+	website: {
+		type: String,
+		get: function(url){
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+					url = "http://" + url;
+				}
+
+				return url;
+			}
+		}
+	},
+	password: {
+		type: String,
+		validate: [
+			function(password){
+				return password.length >= 6;
+			},
+			'Password should be longer'
+		]	
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	}
 });
+
+UserSchema.virtual('fullName').get(function(){
+	return this.firstName + ' ' + this.lastName;
+}).set(function(fullName){
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', {getters: true, virtuals: true});
+
+UserSchema.statics.findOneByUsername = function(username, callback){
+	this.findOne({username: new RegExp(username, 'i')}, callback);
+}
 
 mongoose.model('User', UserSchema);
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -7,9 +7,12 @@ module.exports = function(app){
 
 	app.route('/users/:userId')
 		.get(users.read)
-		.put(users.update);
+		.put(users.update)
+		.delete(users.delete);
 
 	app.param('userId', users.userByID);
+	// this tells the router to use the users.userByID function before executing any other piece of middleware registered wtih the userId parameter
+
 };
 
-// this is going to use the controller for
+// this is going to use the controller for users
